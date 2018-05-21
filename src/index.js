5ed1890ca8582a4f22fa03883640d36bf45719a3
suwimol <kamlangjai.s@northeastern.edu>
2018-05-20 21:27:22 -0400

    added the hello components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import HelloWorld from './hello'
 
 ReactDOM.render(
-    <h1>Hello World!</h1>,
+    <HelloWorld/>,
     document.getElementById('root')
-)
+);
